498cbef7fc7bb09111008d2d956ab9c27f5f76d4
Grigoriev Vlad <grigorievva@soft-division.ru>
2024-10-27 21:45:25 +0300

    добавил слои для разных типов страниц

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtLayout name="bilboard">
+      <template #header> Some header template content. </template>
+      The rest of the page
+      <main name="main"></main>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
