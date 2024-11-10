8f722093fda42fccbe3df50f1f01650573ea31a5
Grigoriev Vlad <grigorievva@soft-division.ru>
2024-11-11 00:44:14 +0300

    добавлены шрифт изменен шаблон входа добавлена картинка

--- pages/auth/SignIn.vue
+++ pages/auth/SignIn.vue
@@ -1,27 +1,62 @@
 <template>
   <NuxtLayout name="auth">
     <template #header>
-      <h1>Мой заголовок</h1>
+      ВХОД
     </template>
 
     <div>
       <div>
 
+        <div class="el-p">
+
+          <el-input v-model="login" style="width: 240px" placeholder="Логин">
+            <template #prefix>
+              <el-icon>
+                <wow-icon type="mdi" :path="$mdi.mdiAccount" />
+              </el-icon>
+            </template>
+          </el-input>
+        </div>
+
+        <div class="el-p">
+          <el-input v-model="password" style="width: 240px" type="password" placeholder="Пароль" show-password>
+            <template #prefix>
+              <wow-icon type="mdi" :path="$mdi.mdiKey" />
+            </template>
+          </el-input>
+        </div>
+
       </div>
-      <el-button type="primary">
-        <el-icon style="vertical-align: middle">
-          <wow-icon type="mdi" :path="$mdi.mdiAccount"></wow-icon>
-        </el-icon>
-        <span style="vertical-align: middle"> Search </span>
-      </el-button>
-      Основной контент ыыыы
+
+      <div class="btn-goup">
+        <el-button type="primary" style="width: 100%" round>
+          <el-icon style="vertical-align: middle">
+            <wow-icon type="mdi" :path="$mdi.mdiAccount"></wow-icon>
+          </el-icon>
+          <span style="vertical-align: middle"> Вход </span>
+        </el-button>
+      </div>
+
     </div>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 const { $mdi } = useNuxtApp();
+
+const login = ref('');
+const password = ref('');
+
 
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-p {
+  padding: 10px 0px;
+}
+
+.btn-goup {
+  padding: 25px 0px;
+}
+</style>
