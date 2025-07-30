b9d11ecb39ad48475127729b80e263df4c06771b
Grigoriev Vlad <grigorievva@soft-division.ru>
2025-07-30 23:36:03 +0300

    fix: merge

--- components/ui/Files/Wow-file-viewer.vue
+++ components/ui/Files/Wow-file-viewer.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="fc fc-row">
-<<<<<<< HEAD
     <div v-for="file in props.files" class="fc fc-row block-file" @click="downloadFile(file.originalName)">
-=======
-    <div v-for="file in props.files" class="fc fc-col block-file" @click="downloadFile(file.originalName)">
->>>>>>> 1c7bf29a06243b3a0a78fcc771cdceb8adc0c202
 
       <el-tooltip :content="`Cкачать - ${file.originalName}`" placement="bottom-end" effect="light">
         <div class="icon-fv fc fc-justify-center">
@@ -12,7 +8,6 @@
         </div>
       </el-tooltip>
 
-<<<<<<< HEAD
       <div class="fc fc-col">
         <el-tooltip :content="file.originalName" placement="bottom-end" effect="light">
           <div class="label-fv">
@@ -21,13 +16,6 @@
         </el-tooltip>
         <div>{{ file.fileSize }}</div>
       </div>
-=======
-      <el-tooltip :content="file.originalName" placement="bottom-end" effect="light">
-        <div class="label-fv">
-          {{ file.originalName }}
-        </div>
-      </el-tooltip>
->>>>>>> 1c7bf29a06243b3a0a78fcc771cdceb8adc0c202
 
 
     </div>
@@ -47,13 +35,10 @@ const props = defineProps({
   },
   folder: {
     type: String
-<<<<<<< HEAD
   },
   typeUrl: {
     type: String,
     default: ''
-=======
->>>>>>> 1c7bf29a06243b3a0a78fcc771cdceb8adc0c202
   }
 })
 
@@ -83,12 +68,8 @@ const getIcoExtension = (originalName: string = '') => {
 // TODO добавить отображение размера файла
 const downloadFile = async (filename: string) => {
   try {
-<<<<<<< HEAD
 
     const response = await $fetch<Blob>(getUrlDownload(props.typeUrl, filename), {
-=======
-    const response = await $fetch<Blob>(`${useRuntimeConfig().public.baseURL}/register-request/file/${props.folder}?filename=${filename}`, {
->>>>>>> 1c7bf29a06243b3a0a78fcc771cdceb8adc0c202
       method: 'GET',
       responseType: 'blob' // Указываем, что ожидаем бинарные данные
     });
@@ -105,7 +86,6 @@ const downloadFile = async (filename: string) => {
   }
 }
 
-<<<<<<< HEAD
 const getUrlDownload = (type: string, filename: string) => {
   let url = '';
   switch (type) {
@@ -122,18 +102,12 @@ const getUrlDownload = (type: string, filename: string) => {
   }
 }
 
-=======
->>>>>>> 1c7bf29a06243b3a0a78fcc771cdceb8adc0c202
 
 </script>
 
 <style lang="less" scoped>
 .block-file {
-<<<<<<< HEAD
   width: 200px;
-=======
-  width: 70px;
->>>>>>> 1c7bf29a06243b3a0a78fcc771cdceb8adc0c202
   cursor: pointer;
   border: 1px solid #f9f9f9;
   border-radius: 5px;
